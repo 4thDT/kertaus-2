06de272f11f1a82d584fbb353ad58759c3470d70
Sygenol <s2300932@edu.bc.figit config --global user.name Sygenol>
2025-02-05 13:22:14 +0200

    jotain

--- index.js
+++ index.js
@@ -1,48 +1,39 @@
 const express = require("express");
-const app = express();
 const path = require("path");
-const dataLayer = require("./datalayer"); // Assuming the datalayer is correctly implemented
+const {
+  getRandomImageCaption,
+  addCaption,
+  changeVotes,
+} = require("./datalayer");
 
-// Set EJS as the view engine and the views folder
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "ejsstuf"));
-
-// Serve static files from the root directory (to serve images and other static assets)
-app.use(express.static(path.join(__dirname)));
-
-// Route for the homepage
-app.get("/", (req, res) => {
-    // Retrieve data from the dataLayer
-    const images = dataLayer.readData();
-    
-    // Render the index.ejs template, passing the image data
-    res.render("index", { images });
-});
-
-// Route for adding a new caption
-app.post("/add-caption", express.urlencoded({ extended: true }), (req, res) => {
-    const { imageName, captionText } = req.body;
-    
-    // Add the new caption to the image
-    dataLayer.addCaption(imageName, captionText);
-    
-    // Redirect back to the homepage
-    res.redirect("/");
-});
-
-// Route for upvoting/downvoting captions
-app.post("/change-votes", express.urlencoded({ extended: true }), (req, res) => {
-    const { imageName, captionNumber, voteChange } = req.body;
-    
-    // Change the vote count for the specified caption
-    dataLayer.changeVotes(imageName, captionNumber, parseInt(voteChange, 10));
-    
-    // Redirect back to the homepage
-    res.redirect("/");
-});
-
-// Start the server on port 3000
+const app = express();
 const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  const { image, caption, captionNumber, votes } = getRandomImageCaption();
+  const imagePath = `${image}`;
+  res.render("index", { image: imagePath, caption, captionNumber, votes });
+});
+
+app.post("/add-caption", (req, res) => {
+  const { imageName, captionText } = req.body;
+  addCaption(imageName, captionText);
+  console.log("Caption added:", captionText);
+  console.log("Image name:", imageName);
+  res.redirect("/");
+});
+
+app.post("/change-votes", (req, res) => {
+  const { imageName, captionNumber, voteChange } = req.body;
+  changeVotes(imageName, captionNumber, parseInt(voteChange));
+  res.redirect("/");
+});
+
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
